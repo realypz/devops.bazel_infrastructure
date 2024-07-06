f1fa2b282ed2c0556f4b22f6f997673f13a82c88
YANG, Peizheng <yang_peizheng@outlook.com>
2024-07-06 13:41:07 +0200

    Move files inside `toolchains/llvm/repos`

--- tests/integration_test_suites/smoke_test.sh
+++ tests/integration_test_suites/smoke_test.sh
@@ -3,5 +3,14 @@
 # Run this script under `tests` directory.
 
 set -e
-bazelisk build --config=llvm_toolchain //...
+
+# Build check
+bazelisk build --config=llvm_toolchain -c opt //...
 bazelisk test --config=llvm_toolchain //...
+bazelisk build --config=clang_tidy //cpp/my_hello:hello
+
+# Format check
+bazelisk run //external_toolchains:bazel_buildifier_fix
+bazelisk run //external_toolchains:clang_format_fix
+bazelisk run //external_toolchains:header_guard -- --workspace-root=$(pwd)
+bazelisk run //external_toolchains:python_black
--- toolchains/llvm/repos/llvm_binaries/repo_rule.bzl
+++ toolchains/llvm/repos/llvm_binaries/repo_rule.bzl
@@ -12,7 +12,7 @@ def _llvm_binaries_impl(rctx):
 
     rctx.template(
         "BUILD.bazel",  # path
-        Label("llvm_binaries/llvm_binaries.BUILD.bazel.tpl"),  # template
+        Label(":internal/llvm_binaries.BUILD.bazel.tpl"),  # template
         substitutions = {
             "@@LLVM_DIR@@": llvm_dir,
         },
